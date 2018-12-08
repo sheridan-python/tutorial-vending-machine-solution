85b8340eca029444a4a3c3890c527c2194bac1fb
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2018-12-08 09:12:49 -0500

    Define the InsufficientFunds exception

---
+++ vending_machine.py
@@ -0,0 +1,11 @@
+"""
+A virtual vending machine.
+"""
+
+
+class InsufficientFunds(Exception):
+    """
+    Exception used to indicate that there isn't
+    enough money to make a purchase.
+    """
+    pass
