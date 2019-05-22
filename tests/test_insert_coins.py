bbdfd7d9b337daf9663ec31b3b677e67c4b19af3
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-05-21 20:23:26 -0400

    Raise ValueError is coin not acceptable

--- tests/test_insert_coins.py
+++ tests/test_insert_coins.py
@@ -1,6 +1,7 @@
 """
 Tests the insert_coin function
 """
+import pytest
 from vending_machine import insert_coin
 
 
@@ -13,3 +14,12 @@ def test_insert_five():
     insert_coin(5, inserted_coins)
 
     assert inserted_coins == [5]
+
+
+def test_insert_fifty():
+    """
+    Given 50, a ValueError should be raised.
+    """
+    inserted_coins = []
+    with pytest.raises(ValueError):
+        insert_coin(50, inserted_coins)
--- vending_machine.py
+++ vending_machine.py
@@ -1,10 +1,15 @@
 """
 A virtual vending machine.
 """
+# A list of coins allowed
+ACCEPTABLE_COINS = [5]
+
 def insert_coin(coin, inserted_coins):
     """
     Accepts a coin and appends it to inserted_coins list.
     """
+    if coin not in ACCEPTABLE_COINS:
+        raise ValueError
     inserted_coins.append(coin)
 
 
