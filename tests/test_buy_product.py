0c7d833bd783b897886618ee4481cae5daf350bd
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-05-22 09:24:37 -0400

    Buying a banana should raise a ValueError

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,6 +1,8 @@
 """
 Tests the buy_product function of the vending machine
 """
+import pytest
+
 from vending_machine import buy_product
 
 
@@ -10,3 +12,13 @@ def test_buy_a_drink():
     the balance returned should be 0.
     """
     assert buy_product('drink', 275) == 0
+
+
+
+def test_buy_banana():
+    """
+    Given that "banana" is purchased, a ValueError should be
+    raised.
+    """
+    with pytest.raises(ValueError):
+        buy_product('banana', 5)  # Random value of 5 balance
